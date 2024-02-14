1e8be9585317ecf827f33bcf990c7c9c93db955a
viktor <viktog2210@gmail.com>
2024-02-14 13:33:07 +0100

    Adding REST endpoint

--- task1/src/components/Calculator.vue
+++ task1/src/components/Calculator.vue
@@ -1,46 +1,47 @@
 <template>
-    <main>
-      <div id="CalculatoroBorder">
-        <div class="input">
-          <NumericInput :displayedNumber="displayedNumber" @input="updateDisplayedNumber"/>
-        </div>
-        <div class="numbers">
-          <NumericButton v-for="num in numbers" :key="num" :value="num" :id="num" @number-clicked="updateDisplayedNumber" />
-          <SymbolButton :value="'.'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
-        </div>
-        <div class="symbol">
-          <Button class="HandlingButton" id="del" @click="removeLast">DEL</Button>
-          <Button class="HandlingButton" id="clear" @click="removeAll">C</Button>
-          <SymbolButton :value="'/'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
-          <SymbolButton :value="'*'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
-          <SymbolButton :value="'-'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
-          <SymbolButton :value="'+'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
-          <Button class="HandlingButton" id="equal" @click="sum" data-testid="equal-button">=</Button>
-        </div>
-        <div class="history">
-          <text>History: </text>
+  <main>
+    <div id="CalculatoroBorder">
+      <div class="input">
+        <NumericInput :displayedNumber="displayedNumber" @input="updateDisplayedNumber" />
+      </div>
+      <div class="numbers">
+        <NumericButton v-for="num in numbers" :key="num" :value="num" :id="num" @number-clicked="updateDisplayedNumber" />
+        <SymbolButton :value="'.'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
+      </div>
+      <div class="symbol">
+        <Button class="HandlingButton" id="del" @click="removeLast">DEL</Button>
+        <Button class="HandlingButton" id="clear" @click="removeAll">C</Button>
+        <SymbolButton :value="'/'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
+        <SymbolButton :value="'*'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
+        <SymbolButton :value="'-'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
+        <SymbolButton :value="'+'" @symbol-clicked="handleSymbolClicked"></SymbolButton>
+        <Button class="HandlingButton" id="equal" @click="sum" data-testid="equal-button">=</Button>
+      </div>
+      <div class="history">
+        <text>History: </text>
         <div id="result">
           <p v-for="(result, index) in calculationResults" :key="index">{{ result }}</p>
         </div>
       </div>
-        <nav>
-          <RouterLink to="/">Calculator</RouterLink>
-          <RouterLink to="/about">Forms</RouterLink>
+      <nav>
+        <RouterLink to="/">Calculator</RouterLink>
+        <RouterLink to="/about">Forms</RouterLink>
       </nav>
-      </div>
-      
-    </main>
-  </template>
+    </div>
+
+  </main>
+</template>
   
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import NumericButton from '@/components/NumericButton.vue';
-  import NumericInput from '@/components/NumericInput.vue';
-  import SymbolButton from '@/components/SymbolButton.vue';
-  
-  const numbers = Array.from({ length: 10 }, (_, index) => index);
+<script setup lang="ts">
+import { ref } from 'vue';
+import axios from 'axios';
+import NumericButton from '@/components/NumericButton.vue';
+import NumericInput from '@/components/NumericInput.vue';
+import SymbolButton from '@/components/SymbolButton.vue';
+
+const numbers = Array.from({ length: 10 }, (_, index) => index);
 const displayedNumber = ref<string | null>(null);
-  const calculationResults = ref<string[]>([]);
+const calculationResults = ref<string[]>([]);
 
 const updateDisplayedNumber = (number: string) => {
   if (displayedNumber.value === null || displayedNumber.value === '0') {
@@ -65,10 +66,34 @@ const removeLast = () => {
 const removeAll = () => {
   displayedNumber.value = null;
 };
-  
-  const hasDivideByZero = (expression: string) => /\/0/.test(expression);
-  
-  const sum = () => {
+
+const hasDivideByZero = (expression: string) => /\/0/.test(expression);
+
+const sum = async () => {
+  let expression = displayedNumber.value;
+  if (expression !== null) {
+    if (hasDivideByZero(expression)) {
+      calculationResults.value.push("ERROR (Can't divide by 0)");
+      displayedNumber.value = null;
+      return;
+    }
+    try {
+      // Replace this with the URL of your Spring Boot application
+      const apiUrl = 'http://localhost:8080/api/calculator/calculate';
+      // Make an HTTP POST request to the backend
+      const response = await axios.post(apiUrl, { expression });
+      // Update the displayedNumber and calculationResults with the response from the backend
+      calculationResults.value.push(`The result of ${expression} is: ${response.data.result}`);
+      displayedNumber.value = String(response.data.result);
+    } catch (error) {
+      // Handle any errors, such as invalid expressions
+      let errorMessage = "ERROR (Invalid expression)";
+      calculationResults.value.push(errorMessage);
+      displayedNumber.value = null;
+    }
+  }
+
+  /*
   let expression = displayedNumber.value;
   if (expression !== null) {
     if (hasDivideByZero(expression)) {
@@ -84,85 +109,85 @@ const removeAll = () => {
       calculationResults.value.push("ERROR (Invalid expression)");
       displayedNumber.value = null;
     }
-  }
+  }*/
 };
-  </script>
+</script>
   
-  <style scoped>
-  .input {
-    grid-area: input;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 200px;
-  }
-  
-  .numbers {
-    grid-area: numbers;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    border: 2px solid gray;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .symbol {
-    grid-area: symbol;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  }
-  
-  .history {
-    grid-area: history;
-    font-size: 20px;
-  }
-  
-  #CalculatoroBorder {
-    border-color: grey;
-    border-width: 2px;
-    border-radius: 10px;
-    border-style: solid;
-    width: 480px;
-    height: 700px;
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    grid-template-areas:
-      'input input input input input input'
-      'numbers numbers numbers numbers symbol symbol'
-      'history history history history history history';
-    grid-gap: 1px;
-    padding: 5px;
-    background-color: gray;
-  }
-  
-  .HandlingButton {
-    width: 100px;
-    height: 40px;
-    background-color: lightblue;
-    color: black;
-    border-radius: 50%;
-  }
-  
-  .HandlingButton:hover {
-    border: 5px solid red;
-  }
-  
-  .NumericInput {
-    padding: 20px 30px;
-    font-size: 24px;
-    font-weight: bold;
-    border: none;
-    border-radius: 5px;
-    border: 2px solid black;
-    text-align: right;
-  }
-  
-  .NumericInput:hover {
-    border-color: #2ecc71;
-  }
-  </style>
+<style scoped>
+.input {
+  grid-area: input;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+}
+
+.numbers {
+  grid-area: numbers;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  border: 2px solid gray;
+  justify-content: center;
+  align-items: center;
+}
+
+.symbol {
+  grid-area: symbol;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.history {
+  grid-area: history;
+  font-size: 20px;
+}
+
+#CalculatoroBorder {
+  border-color: grey;
+  border-width: 2px;
+  border-radius: 10px;
+  border-style: solid;
+  width: 480px;
+  height: 700px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-areas:
+    'input input input input input input'
+    'numbers numbers numbers numbers symbol symbol'
+    'history history history history history history';
+  grid-gap: 1px;
+  padding: 5px;
+  background-color: gray;
+}
+
+.HandlingButton {
+  width: 100px;
+  height: 40px;
+  background-color: lightblue;
+  color: black;
+  border-radius: 50%;
+}
+
+.HandlingButton:hover {
+  border: 5px solid red;
+}
+
+.NumericInput {
+  padding: 20px 30px;
+  font-size: 24px;
+  font-weight: bold;
+  border: none;
+  border-radius: 5px;
+  border: 2px solid black;
+  text-align: right;
+}
+
+.NumericInput:hover {
+  border-color: #2ecc71;
+}
+</style>
   
